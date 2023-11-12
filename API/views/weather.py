4e8e6fed382ef6e7e3aa8af29391a79309da9e8a
Rubayet Mahmud <rubayet.mahmud57@gmail.com>
2023-11-12 13:35:11 +0600

    district name added

--- API/views/weather.py
+++ API/views/weather.py
@@ -13,10 +13,13 @@ class CoolWeather7d64D(APIView):
         obj_list = list(District.objects.values_list(
             "name", "lat", "long"
         ))
+        district = list()
         lat = list()
         long = list()
         for obj in obj_list:
+            district.append(obj[0])
             lat.append(float(obj[1]))
             long.append(float(obj[2]))
         temp_list = get_temperature(lat, long)
-        return Response(temp_list, status=200)
+        result_dict = dict(zip(district, temp_list))
+        return Response(result_dict, status=200)
